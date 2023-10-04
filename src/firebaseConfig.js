2f9a1a08472faf63a9bd4e9b933b90bc5841364b
hardiksuthar74 <sutharhardik747@gmail.com>
2023-10-04 15:26:59 +0530

    added .env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,14 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB2KjXXxqajUwotsu5D1IboLa12cu4VIfo",
-  authDomain: "chat-room-ed095.firebaseapp.com",
-  projectId: "chat-room-ed095",
-  storageBucket: "chat-room-ed095.appspot.com",
-  messagingSenderId: "906184367212",
-  appId: "1:906184367212:web:f5f2f521c478900a04cc49",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
